722c91d3e24ec06248f7de82a7166b08638a044c
Bingham-Dennis <bin12011@byui.edu>
2019-05-01 20:07:59 -0600

    changes to test

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
-<?php
-  echo '<h1>Hello World</h1>'
-  //phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello World App</title>
+    <link rel="stylesheet" type="text/css" href="/stylesheet.css">
+  </head>
+  <body>
+    <div .class="hello">
+        <?php
+          echo "<h1>Hello World</h1>"
+        ?>
+    </div>
+  </body>
+</html>
