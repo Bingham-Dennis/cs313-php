1e9edc59f7a0542380d2d28e45c0d7637aed8c4c
Bingham-Dennis <bin12011@byui.edu>
2019-05-07 20:41:11 -0600

    testing ajax request

--- web/week2/Personal_Homepage/myjs.js
+++ web/week2/Personal_Homepage/myjs.js
@@ -2,7 +2,7 @@ function loadTime() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById('time').innerHTML = new Date(this.responseText + '000');
+      document.getElementById('time').innerHTML = new Date(parseInt(this.responseText) + '000');
     }
   };
   xhttp.open("GET", "./getTime.php", true);
