ffd1b3b21c3c47e7221b6f29b9e13ace7f25f264
Bingham-Dennis <bin12011@byui.edu>
2019-05-07 22:41:13 -0600

    css test

--- web/week2/Personal_Homepage/index.php
+++ web/week2/Personal_Homepage/index.php
@@ -19,35 +19,13 @@
       <div class="innerContainer">
          <div class="item">
             <h3>Assignments</h3>
-            <ul>
-              <li><a href="#">Shopping Cart</a></li>
-              <li><a href="#">DB Setup</a></li>
-              <li><a href="#">DB Access</a></li>
-              <li><a href="#">DB Update</a></li>
-              <li><a href="#">Project 1 Completion</a></li>
-              <li><a href="#">Hello World</a></li>
-              <li><a href="#">Postal Rate Calculator</a></li>
-              <li><a href="#">Milestone 1</a></li>
-              <li><a href="#">Milestone 2</a></li>
-              <li><a href="#">Milestone 3</a></li>
-              <li><a href="#">Project Self Assessment</a></li>
+            <ul id="list1">
             </ul>
          </div>
           <div class="item">
             <h3>Team Assignments</h3>
-            <ul>
-                <li><a href="../Team_Assignment/index.html">Week 2</a></li>
-                <li><a href="#">Week 3</a></li>
-                <li><a href="#">Week 4</a></li>
-                <li><a href="#">Week 5</a></li>
-                <li><a href="#">Week 6</a></li>
-                <li><a href="#">Week 7</a></li>
-                <li><a href="#">Week 8</a></li>
-                <li><a href="#">Week 9</a></li>
-                <li><a href="#">Week 10</a></li>
-                <li><a href="#">Week 11</a></li>
-                <li><a href="#">Week 12</a></li>
-                <li><a href="#">Week 13</a></li>
+            <ul id="list2">
+
             </ul>
           </div>
       </div>
--- web/week2/Personal_Homepage/myjs.js
+++ web/week2/Personal_Homepage/myjs.js
@@ -8,3 +8,204 @@ function loadTime() {
   xhttp.open("GET", "./getTime.php", true);
   xhttp.send();
 }
+
+/******************************************************************************
+ *  Add list Items
+ * This function will add the navigation links to the lists
+ *****************************************************************************/
+function addNavItems() {
+  // create the array of items in the lists
+  let listItems1 = [
+    'Shopping Cart',
+    'DB Setup',
+    'DB Access',
+    'DB Update',
+    'Project 1 Completion',
+    'Hello World',
+    'Postal Rate Calculator',
+    'Milestone 1',
+    'Milestone 2',
+    'Milestone 3',
+    'Project Self Assessment'
+  ];
+
+  let listItems2 = [
+    'Week 2',
+    'Week 3',
+    'Week 4',
+    'Week 5',
+    'Week 6',
+    'Week 7',
+    'Week 8',
+    'Week 9',
+    'Week 10',
+    'Week 11',
+    'Week 12',
+    'Week 13'
+  ];
+
+  // loop over the array using a for each loop.
+  listItems1.forEach((item) => {
+      // create a list item element.
+      let li = document.createElement('li');
+
+      //create an anchor tag element.
+      let a = document.createElement('a');
+
+      // add the text from the item we got from the array
+      // to the anchor tag element we just created.
+      a.innerHTML = item;
+
+      // get the url and set it to the href attribute on
+      // the anchor tag.
+      a.setAttribute('href', getURL(item));
+
+      // add the anchor tag to the list element wer created
+      // earlier.
+      li.appendChild(a);
+
+      // add the list item to the un-ordered list with the
+      // id nav_items.
+      document.getElementById('list1').appendChild(li);
+  });
+
+  // loop over the array using a for each loop.
+  listItems2.forEach((item) => {
+    // create a list item element.
+    let li = document.createElement('li');
+
+    //create an anchor tag element.
+    let a = document.createElement('a');
+
+    // add the text from the item we got from the array
+    // to the anchor tag element we just created.
+    a.innerHTML = item;
+
+    // get the url and set it to the href attribute on
+    // the anchor tag.
+    a.setAttribute('href', getURL(item));
+
+    // add the anchor tag to the list element wer created
+    // earlier.
+    li.appendChild(a);
+
+    // add the list item to the un-ordered list with the
+    // id nav_items.
+    document.getElementById('list2').appendChild(li);
+});
+}
+
+/******************************************************************************
+*  Get URL
+* This function will return the correct URL based on the listItem or item
+* since we pulled it out of the array in the for loop.
+* @param: link
+*****************************************************************************/
+function getURL(link) {
+  const items = [
+    {
+      name: 'Home',
+      url: 'index.html'
+    },
+    {
+      name: 'Shopping Cart',
+      url: ''
+    },
+    {
+      name: 'DB Setup',
+      url: ''
+    },
+    {
+      name: 'DB Access',
+      url: ''
+    },
+    {
+      name: 'DB Update',
+      url: ''
+    },
+    {
+      name: 'Project 1 Completion',
+      url: ''
+    },
+    {
+      name: 'Hello World',
+      url: ''
+    },
+    {
+      name: 'Postal Rate Calculator',
+      url: ''
+    },
+    {
+      name: 'Milestone 1',
+      url: ''
+    },
+    {
+      name: 'Milestone 2',
+      url: ''
+    },
+    {
+      name: 'Milestone 3',
+      url: ''
+    },
+    {
+      name: 'Project Self Assessment',
+      url: ''
+    },
+    {
+      name: 'Week 2',
+      url: '../Team_Assignment/index.html'
+    },
+    {
+      name: 'Week 3',
+      url: ''
+    },
+    {
+      name: 'Week 4',
+      url: ''
+    },
+    {
+      name: 'Week 5',
+      url: ''
+    },
+    {
+      name: 'Week 6',
+      url: ''
+    },
+    {
+      name: 'Week 7',
+      url: ''
+    },
+    {
+      name: 'Week 8',
+      url: ''
+    },
+    {
+      name: 'Week 9',
+      url: ''
+    },
+    {
+      name: 'Week 10',
+      url: ''
+    },
+    {
+      name: 'Week 11',
+      url: ''
+    },
+    {
+      name: 'Week 12',
+      url: ''
+    },
+    {
+      name: 'Week 13',
+      url: ''
+    }
+  ];
+
+  items.forEach((item) => {
+    if (item.name === link && item.url != ''){
+      return item.url;
+    } else {
+      return '#';
+    }
+  });
+}
