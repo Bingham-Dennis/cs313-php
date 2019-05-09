365bd3c4adf3427f7fbb1948220f1e707f460709
Bingham-Dennis <bin12011@byui.edu>
2019-05-09 08:04:07 -0600

    adding Team Assignment 3

--- web/week3/process.php
+++ web/week3/process.php
@@ -7,18 +7,13 @@
       $comment = $_POST["comment"];
       $countryMessage = "<p>You have been to ";
       $countries = $_POST['countryList'];
-      var_dump($name);
-      var_dump($email);
-      var_dump($major);
-      var_dump($comment);
-      var_dump($countries);
       print "<h1>Welcome $name</h1><br>";
       print "<h3>your email is $email</h3><br>";
       print "<h5>you are a $major major</h5><br>";
       print "<h5>$comment</h5><br>";
       foreach($countries as $country) {
         if ($country === end($countries )) {
-          $countryMessage = $countryMessage . $country;
+          $countryMessage = "and " . $countryMessage . $country;
         } else {
           $countryMessage = $countryMessage . $country . ", ";
         }
