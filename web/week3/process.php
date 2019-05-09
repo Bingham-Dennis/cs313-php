482b744bbe6c452dde4805957ba0b64377513815
Bingham-Dennis <bin12011@byui.edu>
2019-05-09 07:47:54 -0600

    adding Team Assignment 3

---
+++ web/week3/process.php
@@ -0,0 +1,18 @@
+<html>
+  <body>
+  <?php
+      $countryMessage = "<p>You have been to ";
+      $countries = $_POST['countryList[]'];
+      echo "<h1>Welcome $_POST["name"]</h1><br>"
+      echo "<h3>your email is $_POST["email"]</h3><br>"
+      echo "<h5>you are a $_POST["major"]</h5><br>"
+      foreach($countries as $country) {
+        if ($country === end($countries )) {
+          $countryMessage = $countryMessage . $country;
+        } else {
+          $countryMessage = $countryMessage . $country . ", ";
+        }
+      }
+  ?>
+  </body>
+</html>
