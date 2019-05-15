8039bfc2c31e555bddab1338858780ab5d9c0e2a
Bingham-Dennis <bin12011@byui.edu>
2019-05-15 08:51:23 -0600

    added store.php to week 3 assignement

--- web/Assignments/week3/store.php
+++ web/Assignments/week3/store.php
@@ -1,6 +1,6 @@
 <?php
   if (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['image']) && isset($_POST['description'])) {
-    $dog = array($_POST['id'], $_POST['name'], $_POST['image'], $_POST['decription']);
+    $dog = array($_POST['id'], $_POST['name'], $_POST['image'], $_POST['description']);
     var_dump($dog);
   } else {
     echo "broken";
