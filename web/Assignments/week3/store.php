fc3f131d915d09475cdfc4a15d6413ecce180656
Bingham-Dennis <bin12011@byui.edu>
2019-05-15 08:11:28 -0600

    added store.php to week 3 assignement

--- web/Assignments/week3/store.php
+++ web/Assignments/week3/store.php
@@ -1,6 +1,7 @@
 <?php
-  session.start();
-  $_SESSION['cart'] = array();
-  push_array($_SESSION['cart'], $_POST['dog']);
-  var_dump($_SESSION['cart']);
+  echo "hello";
+  // session.start();
+  // $_SESSION['cart'] = array();
+  // push_array($_SESSION['cart'], $_POST['dog']);
+  // var_dump($_SESSION['cart']);
 ?>
