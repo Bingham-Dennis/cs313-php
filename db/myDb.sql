eedbfc5e127d8f1031ab1fe656eb1f7a20d7c9ce
Bingham-Dennis <bin12011@byui.edu>
2019-05-21 21:53:49 -0600

    added database tables and commands here

---
+++ db/myDb.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users(user_id SERIAL PRIMARY KEY, fName varchar(50) NOT NULL, lName varchar(50) NOT NULL, username varchar(50) NOT NULL, email varchar(100) NOT NULL);
+
+CREATE TABLE subusers(sub_user_id SERIAL PRIMARY KEY, user_id int REFERENCES users(user_id) NOT NULL, fName varchar(50) NOT NULL, username varchar(50) NOT NULL);
+
+CREATE TABLE movies(movie_id SERIAL PRIMARY KEY, user_id int REFERENCES users(user_id) NOT NULL, title varchar(50) NOT NULL, rating varchar(10) NOT NULL, synopsis text, artwork varchar(100), movie_file varchar(100) NOT NULL, file_ext varchar(20) NOT NULL);
+
+CREATE TABLE restrictions(restriction_id SERIAL PRIMARY KEY, user_id int REFERENCES users(user_id) NOT NULL, sub_user_id int REFERENCES subusers(sub_user_id) NOT NULL, rating varchar(10) NOT NULL, media_type varchar(20) NOT NULL);
