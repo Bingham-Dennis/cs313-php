76056a2ebe32c8e1dd0ec68ea28dab0fea168de4
Bingham-Dennis <bin12011@byui.edu>
2019-06-03 15:36:05 -0600

    added week5 assignment

--- web/Assignments/week5/connect.php
+++ web/Assignments/week5/connect.php
@@ -43,7 +43,7 @@
 
     if(isset($_SESSION['Logged_in']) && $_SESSION['Logged_in'] === true) {
       $_SESSION['user'] = $givenUsername;
-      header('Location: dashboard.html');
+      header("Location: dashboard.html");
     }
 
   } else {
