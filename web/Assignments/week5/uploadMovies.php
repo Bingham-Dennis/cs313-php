24fb1d51dc92fbd170e0aeb7b56dddf9c1a1c65e
Bingham-Dennis <bin12011@byui.edu>
2019-06-04 17:29:00 -0600

    updating week 5

--- web/Assignments/week5/uploadMovies.php
+++ web/Assignments/week5/uploadMovies.php
@@ -54,35 +54,6 @@
   }
 ?>
 
-<?php
-   try
-   {
-     session_name('MoviesPlus');
-     session_start();
-     $dbUrl = getenv('DATABASE_URL');
-
-     $dbOpts = parse_url($dbUrl);
-
-     $dbHost = $dbOpts["host"];
-     $dbPort = $dbOpts["port"];
-     $dbUser = $dbOpts["user"];
-     $dbPassword = $dbOpts["pass"];
-     $dbName = ltrim($dbOpts["path"],'/');
-
-     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   }
-   catch (PDOException $ex)
-   {
-     echo 'Error!: ' . $ex->getMessage();
-     die();
-   }
-
-   var_dump($_SESSION['user']);
-   var_dump($_SESSION['user_id']);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
